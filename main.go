1b786cecfef519c030bf7c68dcb8e16666689aae
lyswifter <ly70835@163.com>
2021-10-29 13:35:37 +0800

    add redirect html

--- main.go
+++ main.go
@@ -15,8 +15,8 @@ const client_id = "Iv1.018aba55453994ac"
 const client_secret = "e6a5b65152a4dca9754fa2e13df80f3c087019e7"
 
 var accessTokenUrl = "https://github.com/login/oauth/access_token"
-var redirect_uri = ""
-var state = ""
+var redirect_uri = "http://47.96.128.154:9091/public/auth/"
+var state = "xxxxxx"
 
 func main() {
 	Infof("This is the internet computer github authorize server")
@@ -38,34 +38,54 @@ func handleGithubAuthorizeAPI(r *gin.Engine) {
 	r.GET("/public/auth", func(c *gin.Context) {
 		code := c.Query("code")
 
-		getAccessToken(code, redirect_uri, state)
+		Infof("get authorize redirect(%s): %s", c.Request.URL.String(), code)
+
+		if code == "" {
+			c.JSON(500, gin.H{
+				"status":  "Err",
+				"message": "could not get auth code",
+			})
+			return
+		}
+
+		err := getAccessToken(code, redirect_uri, state)
+		if err != nil {
+			Errorf("get access token: %s", err.Error())
+			return
+		}
+
+		// r.LoadHTMLFiles("index.html")
+
+		c.HTML(200, "index.html", nil)
 	})
 }
 
-func getAccessToken(code string, redirect_uri string, state string) {
+func getAccessToken(code string, redirect_uri string, state string) error {
 	url := fmt.Sprintf("%s?client_id=%s&client_secret=%s&code=%s&redirect_uri=%s&state=%s", accessTokenUrl, client_id, client_secret, code, redirect_uri, state)
 	method := "POST"
+
+	Infof("access_token url: %s", url)
 
 	client := &http.Client{}
 	req, err := http.NewRequest(method, url, nil)
 
 	if err != nil {
-		fmt.Println(err)
-		return
+		Errorf("new request err: %s", err.Error())
+		return err
 	}
 	res, err := client.Do(req)
 	if err != nil {
-		fmt.Println(err)
-		return
+		Errorf("do request err: %s", err.Error())
+		return err
 	}
 	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		fmt.Println(err)
-		return
+		Errorf("read response err: %s", err.Error())
+		return err
 	}
-	fmt.Println(string(body))
+	Infof("request access token response: %s", string(body))
 
 	err = SaveAccessToken(context.TODO(), state, AuthResponse{
 		AccessToken:          "",
@@ -77,6 +97,8 @@ func getAccessToken(code string, redirect_uri string, state string) {
 	})
 	if err != nil {
 		Errorf("save access token err %s", err.Error())
-		return
+		return err
 	}
+
+	return nil
 }
